85117110a12655f0f4a478e81f375d81abb59689
CamilleBargoin <cbargoin@gmail.com>
2016-07-14 21:06:11 +0200

    rebuild

--- server.bundle.js
+++ server.bundle.js
@@ -542,7 +542,7 @@
 	module.exports = {
 	    "production": {
 	        "api": "http://minibook-express.herokuapp.com",
-	        "websocket": "http://minibook-express.herokuapp.com"
+	        "websocket": "http://minibook-express.herokuapp.com:80"
 	    },
 	    "development": {
 	        "api": "http://localhost:3000",
